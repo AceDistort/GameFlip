21f271c200a9f7933512e6d6ccb5143db35e27e9
adavid <antonin.david79@gmail.com>
2023-09-07 11:10:52 +0200

    Add custom 404 and 403 erros

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,33 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Game;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->hasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        // injection de l'admin
+        $admin = new User();
+        $admin->setEmail('admin@mail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->hasher->hashPassword($admin, 'Super@dmin1234'));
+        $admin->setFirstName('Admin');
+        $admin->setLastName('Super');
+        $manager->persist($admin);
+
+
         $category1 = new Category();
         $category1->setName('Party');
         $category1->setColor('#FF0000');
@@ -57,7 +77,7 @@ class AppFixtures extends Fixture
         $game1 = new Game();
         $game1->setName('WereWolf');
         $game1->setDescription('WereWolf is a game of deception. Each player is secretly assigned a role - Werewolf, Villager, or Seer (a special Villager). There is also a Moderator who controls the flow of the game. The game alternates between night and day phases. At night, the Werewolves secretly choose a Villager to kill. Also, the Seer (if still alive) asks whether another player is a Werewolf or not. During the day, the Villager who was killed is revealed and is out of the game. The remaining Villagers then vote on the player they suspect is a Werewolf. That player reveals his/her role and is out of the game. Werewolves win when there are an equal number of Villagers and Werewolves. Villagers win when they have killed all Werewolves. Werewolf is a social puzzle game.');
-        $game1->setImage('https://cdn.1j1ju.com/thumbs/game-lg/medias/4c/ee/72-les-loups-garous-de-thiercelieux-2019-cover.jpeg');
+        $game1->setImage('72-les-loups-garous-de-thiercelieux-2019-cover-64f87efb3f659.jpg');
         $game1->addCategory($category1);
         $game1->addCategory($category2);
         $game1->addCategory($category6);
@@ -67,7 +87,7 @@ class AppFixtures extends Fixture
         $game2 = new Game();
         $game2->setName('Top Ten');
         $game2->setDescription('A game about making lists');
-        $game2->setImage('https://joueclub-joueclub-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/b59eae42202d05160fe5e46967dae7bb83f5aaf1_06029346.jpeg');
+        $game2->setImage('Top-ten-face-logo-BD-788x1024-64f885a4e2a69.jpg');
         $game2->addCategory($category1);
         $game2->addCategory($category3);
 
@@ -76,7 +96,7 @@ class AppFixtures extends Fixture
         $game3 = new Game();
         $game3->setName('Dixit');
         $game3->setDescription('A game about making up stories');
-        $game3->setImage('https://shop.oikaoika.fr/Module/Catalogue/ImgProd.php5?ID=219&Big=1');
+        $game3->setImage('0393061-0-64f885cff33c4.jpg');
         $game3->addCategory($category1);
         $game3->addCategory($category4);
 
@@ -85,7 +105,7 @@ class AppFixtures extends Fixture
         $game4 = new Game();
         $game4->setName('Dobble');
         $game4->setDescription('A game about finding matching symbols');
-        $game4->setImage('https://cdn2.philibertnet.com/494047-thickbox_default/dobble.jpg');
+        $game4->setImage('dobble-boite-64f885ea79d12.jpg');
         $game4->addCategory($category1);
         $game4->addCategory($category5);
 
@@ -93,7 +113,7 @@ class AppFixtures extends Fixture
 
         $game5 = new Game();
         $game5->setName('TAC-TIK');
-        $game5->setImage('https://image.jimcdn.com/app/cms/image/transf/dimension=1820x1280:format=jpg/path/sd22ff8e5d8ddc8c3/image/i01c75c16ccc46438/version/1633080251/image.jpg');
+        $game5->setImage('image-64f88609281e5.jpg');
         $game5->addCategory($category2);
         $game5->addCategory($category3);
 
